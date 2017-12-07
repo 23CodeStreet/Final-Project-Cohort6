2afe5fdf7e9e181c854ec5292a433f075f0150a1
Han <Han@Hannahs-MacBook-Pro.local>
2017-12-07 21:32:50 +0000

    partly working code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 /* ---
 CODE TO GRAB THE VALUE FROM THE RADIO BUTTON -
-- CURRENTLY NOT BEING USED FOR ANYTHING!
 --- */
 function getRadioValue() {
     var radioValue = $("input[name='type']:checked").val();
-    console.log(radioValue);
 
     // Return something if you wish to
     return radioValue;
@@ -12,20 +10,38 @@ function getRadioValue() {
 
 
 // Starts the search function. API calls will run asynchronously so it is best to split your function into 2 parts.
+
 function startSearch() {
 
+    var type = getRadioValue();
+
     var searchTerm = $('input[name=searchName]').val();
-    // Array of types
-    var types = ["movie", "person", "tv"];
+
+      if(searchTerm != '') {
+          makeSearch(type);
+      };
+
+
+
+    // This will be replacing the above array.
+
 
     // For each type in array, make a search
-    for (var i = 0; i < types.length; i++) {
-
-        // Check that the search term isn't blank
-        if(searchTerm != '') {
-            makeSearch(types[i]);
-        }
-    }
+    // for (var i = 0; i < types.length; i++) {
+    //
+    //     // Check that the search term isn't blank
+    //     if(searchTerm != '') {
+    //         makeSearch(types[i]);
+    //     }
+    // // }
+    // //
+    // // for (var i = 0; i < types.length; i++) {
+    // //
+    // //     // Check that the search term isn't blank
+    // //     if(searchTerm != 'person') {
+    // //         makeSearch(types[i]);
+    // //     }
+    // }
 
 }
 
@@ -41,14 +57,36 @@ function makeSearch(type) {
             // Check on the type and grab title / name respectively
             if (type == "movie") {
                 m.title = data.results[j].title;
-            } else if (type == "person" || type == "tv") {
-                m.title = data.results[j].name;
+            } else if (type == "person") {
+                m.title = data.results[0].known_for[j].title;
             }
             // Print type and title out to the screen
-            console.log(type + ' ' + m.title);
+            console.log(type + ' ' + m.title + ' ' + data.results[j].id);
         }
+        //
+        // for (var j = 0; j < data.results.length; j++) {
+        //
+        //     var m = {}
+        //
+        //     if (type == "person") {
+        //         m.title = data.results[0].known_for[0].title;
+        //     } else if ()
+        //     console.log(type + ' ' + m.title + ' ' + data.results[0].id);
+        // }
     });
 }
+
+// get movies with actor/person
+
+
+   // $('input[name=searchInput]').change(function(e){
+   //   $.get('https://api.themoviedb.org/3/search/person?&api_key=48ca54ddb0a0259e28dbb2afa690ed61&query=' + encodeURIComponent(e.target.value), function(data){
+   //     console.log(data.results[0].known_for)
+   //   })
+   // })
+
+
+
 
 // This is just test code for now - we will get this working on Thursday.
 // function getGenres() {
