7b65ad7f328b5974d809c9141ccc8ea6b1929d13
aboutjenny <jenny.dejesus@hotmail.com>
2017-12-11 21:33:09 +0000

    Results working with real data

--- js/script.js
+++ js/script.js
@@ -58,8 +58,11 @@ function makeSearch(type) {
 
 function moviesByName(type) {
   $.get('https://api.themoviedb.org/3/search/movie' + '?&api_key=48ca54ddb0a0259e28dbb2afa690ed61&query=' + encodeURIComponent($('input[name=searchName]').val()), function(data) {
+    $('.movieResults').replaceWith("<div class='movieResults'></div>")
     // console.log(data)
-
+    for (var i = 0; i < data.results.length; i++) {
+    $('.movieResults').append(createResults(data.results[i]));
+    }
     })
 };
 
@@ -67,30 +70,76 @@ function moviesByName(type) {
 
 function moviesByActor(id) {
     $.get('https://api.themoviedb.org/3/discover/movie' + '?&api_key=48ca54ddb0a0259e28dbb2afa690ed61&with_cast=' + id, function(movies){
+      $('.movieResults').replaceWith("<div class='movieResults'></div>")
       console.log(movies)
       for (var i = 0; i < movies.results.length; i++) {
-        console.log(movies.results[i].title);
+      $('.movieResults').append(createResults(movies.results[i]));
       }
     })
 };
 
+function getGenres() {
+
+  // Create a variable array that is empty
+  var genreList = [];
+
+  // if ("16".checked==true){
+  //   // console.log(animation);
+  // }
+
+  // Get values from only *checked* checkboxes
+  $.each($("input[name='genre']:checked"), function() {
+    // push the values into an array
+    genreList.push($(this).val());
+  });
+
+  // create a for loop to run as many itmes as there are items in the array
+  for (var i = 0; i < genreList.length; i++) {
+    console.log(genreList[i]);
+    searchByGenre(genreList[i]);
+  }
+
+  // console.log(genreList.join(", "));
+
+  // if(genreList != '') {
+  //     searchByGenre(genreList[0]);
+  // }
 }
+
+function searchByGenre(genre) {
+  $.get('https://api.themoviedb.org/3/genre/' + genre + '/movies?api_key=48ca54ddb0a0259e28dbb2afa690ed61', function(data) {
+    $('.movieResults').replaceWith("<div class='movieResults'></div>")
+    // $('.resultContainer').replaceWith('<div class="resultContainer"></div>')
+    for (var i = 0; i < data.results.length; i++) {
+      console.log(genre);
+      console.log(data.results[i].title);
+      for (var i = 0; i < data.results.length; i++) {
+        $('.movieResults').append(createResults(data.results[i]));
+      }
+
+      // $('body').append(data.results[i].title);
+      // $('.resultContainer').append(createHTML(data.results[i]))
+
+    }
+  })
+}
+
 
 // RESULTS HTML
 
-for(var i = 0; i < 24; i++) {
-   $('.movieResults').append(createResults());
-   console.log ("loop is working")
-}
+// for(var i = 0; i < 24; i++) {
+//    $('.movieResults').append(createResults());
+//
+// }
 
-function createResults() {
-
+function createResults(data) {
+console.log(data)
    var container = '<div class="movieContainer">';
    // var image = '<img src="img/placeholder.png" class="poster">';
-   var image = '<img src="http://www.joblo.com/timthumb.php?src=/posters/images/full/moonlight-poster-lg.jpg&h=600&q=100" class="poster">'
+   var image = '<img src="https://image.tmdb.org/t/p/w500' + data.poster_path + '" class="poster">'
    var titleContainer = '<div class="titleContainer">'
-   var title = '<h3 class="movieTitle"> movie title</h3>'
-   var description = "<p class='movieDesciption'> Fueled by his restored faith in humanity and inspired by Superman's selfless act, Bruce Wayne enlists the help of his newfound ally, Diana Prince, to face an even greater enemy.</p>"
+   var title = '<h3 class="movieTitle">' + data.title + '</h3>'
+   var description = "<p class='movieDesciption'>" + data.overview +"</p>"
    var readMore = '<a href="#" class="readMoreButton"> Read more </a>'
    var closeDiv = '</div>';
 
@@ -102,17 +151,17 @@ function createResults() {
 }
 
 
-function getSimilarMovies () {
-      $.get('https://api.themoviedb.org/3/movie/' + '123' + '/similar?&api_key=48ca54ddb0a0259e28dbb2afa690ed61',
-      function(data){
-        console.log(data.results)
-        for(var i = 0; i < data.results.length; i++) {
-           console.log (data.results[i])
-        }
-      })
-}
-
-getSimilarMovies()
+// function getSimilarMovies () {
+//       $.get('https://api.themoviedb.org/3/movie/' + '123' + '/similar?&api_key=48ca54ddb0a0259e28dbb2afa690ed61',
+//       function(data){
+//         console.log(data.results)
+//         for(var i = 0; i < data.results.length; i++) {
+//            console.log (data.results[i])
+//         }
+//       })
+// }
+//
+// getSimilarMovies()
 
 
 
@@ -139,4 +188,3 @@ getSimilarMovies()
 //         }
 //     })
 // }
-
